b4eca319b8a18d0c574f4682f1319a6a66aba2ed
Bill Caldwell <xiaolinslacker@gmail.com>
2018-11-04 21:19:43 -0600

    Initial commit

---
+++ Assets/Code/Game.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	public static GameObject Instance;
+
+	// Use this for initialization
+	void Start () {
+
+		if (null == Instance) {
+			Instance = this.gameObject;
+		} else {
+			Destroy(this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
